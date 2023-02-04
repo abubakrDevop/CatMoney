a4f9be967cecfd5f9a04bb5bbf6b22f50b93f12b
Abubakr <sherzodabubakr@gmail.com>
2023-02-04 17:15:18 +0600

    new up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Index } from "./components";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/*" element={<Index />} />
+        <Route index path="/*" element={''} />
       </Routes>
     </BrowserRouter>
   );
