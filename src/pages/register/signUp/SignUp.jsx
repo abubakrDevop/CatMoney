9e9adfab86d1aed03114fbe44db45a91d44b31d9
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-05 11:06:06 +0200

    move from redux to redux toolkit

--- src/pages/register/signUp/SignUp.jsx
+++ src/pages/register/signUp/SignUp.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+
 import cls from '../signUp/SignUp.module.scss'
 import { useForm } from 'react-hook-form'
 import { Form } from '../../../helpers/form/index'
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import  { 
-          IoAtOutline, 
+import  {
+          IoAtOutline,
           IoEyeOffOutline,
           IoEyeOutline,
           IoLockClosedOutline,
           IoPersonOutline,
         } from 'react-icons/io5'
+import {addUser} from "../../../store/userSlice";
+import {useState} from "react";
 
 export const SignUp = () => {
-  const [active, setActive] = React.useState(false)
-  const [error, setError] = React.useState('')
+  const [active, setActive] = useState(false)
+  const [error, setError] = useState('')
   const dispatch = useDispatch()
-  const inputData = useSelector(state => state.inputData.inputData)
-  const wallets = useSelector(state => state.wallets.wallets)
+  const {inputData} = useSelector(state => state.user)
   console.log("inputData", inputData)
 
   const {
@@ -41,7 +42,7 @@ export const SignUp = () => {
         if (res.data.status === '200') {
           reset()
           localStorage.setItem("regist", JSON.stringify(res.data))
-          dispatch({type: 'ADD_USER', payload: body})
+          dispatch(addUser(body))
           setTimeout(() => {window.location.reload()}, 3000)
         } else if ('Такой пользователь уже зарегистрирован!') {
           reset()
@@ -66,9 +67,9 @@ export const SignUp = () => {
 
         <div className={cls.form_inputbox}>
           <IoPersonOutline className={cls.input_icon} />
-          <input 
-            className={cls.form_input} 
-            type="text" 
+          <input
+            className={cls.form_input}
+            type="text"
             placeholder="Придумайте логин..."
             {...register('login', Form.Options.allInputs)}
           />
@@ -80,9 +81,9 @@ export const SignUp = () => {
 
         <div className={cls.form_inputbox}>
           <IoAtOutline className={cls.input_icon} />
-          <input 
-            className={cls.form_input} 
-            type="email" 
+          <input
+            className={cls.form_input}
+            type="email"
             placeholder="Введите email..."
             {...register('email', Form.Options.email)}
           />
@@ -94,21 +95,21 @@ export const SignUp = () => {
 
         <div className={cls.form_inputbox}>
           <IoLockClosedOutline className={cls.input_icon} />
-          <input 
-            className={cls.form_input} 
-            type={active === false ? 'password' : 'text'} 
+          <input
+            className={cls.form_input}
+            type={active === false ? 'password' : 'text'}
             placeholder="Придумайте пароль..."
             {...register('password', Form.Options.password)}
           />
           {
-            active === false ? 
-            <IoEyeOffOutline 
-              className={cls.input_eye} 
-              onClick={() => setActive(true)} 
+            active === false ?
+            <IoEyeOffOutline
+              className={cls.input_eye}
+              onClick={() => setActive(true)}
             />
             :
-            <IoEyeOutline 
-              className={cls.input_eye} 
+            <IoEyeOutline
+              className={cls.input_eye}
               onClick={() => setActive(false)}
             />
           }
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import {configureStore} from "@reduxjs/toolkit";
+import user from "./userSlice";
+import wallets from "./walletSlice";
+
+export const store = configureStore({
+	reducer:{
+		user,
+		wallets
+	}
+})
