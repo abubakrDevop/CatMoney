39a279cf98528f1abceb470d02cf26f2fdb9deac
dutyfgh <kastrula09@gmail.com>
2023-07-07 15:45:40 +0300

    adapted login in page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -5,37 +5,37 @@ export const Register = () => {
   return (
     <div className={cls.register}>
       <div className={cls.registerBlock}>
-        
-          <h3>Добро пожаловать</h3>
-          <span  className={cls.welcome}>Добро пожаловать! Пожалуйста, введите свои данные.</span>
 
-          <form>
-            <div className={cls.email}>
-              <p>Login</p>
-              <input type="text" placeholder='Введите свой login' />
-            </div>
+        <h3>Добро пожаловать</h3>
+        <span className={cls.welcome}>Добро пожаловать! Пожалуйста, введите свои данные.</span>
 
-            <div className={cls.password}>
-              <p>Password</p>
-              <input type="password" placeholder='Введите свой пароль' />
-            </div>
-
-            <a href='' className={cls.forgotPassword} >Забыл пароль</a><br />
-            <button className={cls.signUp}>войти</button>
-          </form>
-
-          <div className={cls.loginIn}>
-            <span>Нету акаунта? <a href="">Зарегестрироватся</a></span>
+        <form>
+          <div className={cls.email}>
+            <p>Login</p>
+            <input type="text" placeholder='Введите свой login' />
           </div>
+
+          <div className={cls.password}>
+            <p>Password</p>
+            <input type="password" placeholder='Введите свой пароль' />
+          </div>
+
+          <a href='' className={cls.forgotPassword} >Забыл пароль</a><br />
+          <button className={cls.signUp}>войти</button>
+        </form>
+
+        <div className={cls.loginIn}>
+          <span>Нету акаунта? <a href="">Зарегестрироватся</a></span>
+        </div>
 
       </div>
 
       <div className={cls.logoBlock}>
         <div className={cls.around}>
           <p><span>cat</span> money</p>
+          <div className={cls.filter}></div>
         </div>
 
-        <div className={cls.filter}></div>
 
       </div>
 
